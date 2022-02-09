ffe12b4bccf61488899a02acd054476e5ccbbb34
Enchanting121 <samantha.barros95@gmail.com>
2022-02-09 00:23:22 -0500

    commit tree bbaee3c6a8bbce420a99a3a6495ceb926ba612d0

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(some_value) {
+    let response
+    if (some_value <= 400) {
+        response = 'This one is on me!';
+    } else if (some_value > 2000 && some_value < 2500) {
+        response = 'I will gladly take your thirty bucks.';
+    } else if (some_value > 2500) {
+        response = 'No can do.';
+    }
+    return response;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+    let response
+    if (destination == 'NYC') {
+        response = 'Ok, sounds good.';
+    } else return 'No go.';
+    return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(manners) {
+    let response
+    if (manners == 'generous') {
+        response = 'Thank you so much.';
+    } else if (manners == 'not as generous') {
+        response = 'Thank you.';
+    } else response = 'Bye.';
+    return response
 }
